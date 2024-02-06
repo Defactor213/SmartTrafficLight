466207171f103a3d9ffe1f29a27507732b156eac
G1-Eric-Li <ericlitong@gmail.com>
2024-02-06 09:50:52 +0800

    update for camera trap STT

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,209 +1,30 @@
 // declare libraries
 #include "Ultrasonic.h"
 
-// 0 Accident/Breakdown
-// 1 Road Vehicles
-// 2 Pedestrians
-// 3 Not Pedestrian or Vehicles
-// 4 Disabled Pedestrians
-// remapping Labels in the mapLabels method (see below)
-
 // declare pins and variables
 Ultrasonic ultrasonic(19);
-const int LEDRed = 11;
-const int LEDGreen = 12;
-const int LEDPurple = A6;
 
 // variable to store distance of ultrasonic sensor
 long RangeInCentimeters;
 
 int range_threshold = 15;
 
-// 0 is LOW, 1 is HIGH, variable to store state of lights
-int LEDGreenState = 0;
-int LEDPurpleState = 0;
-int LEDRedState = 0;
-
 // setting up hardware connections
 void setup()
 {
-    pinMode(LEDRed, OUTPUT);
-    pinMode(LEDPurple, OUTPUT);
-    pinMode(LEDGreen, OUTPUT);
     Serial.begin(9600);
 }
-
-// store previous class_index
-int prev_class_index = -1;
-
-// previous range value
-int prev_range_index = -1;
-
-// store duration of amber light in ms
-int amber_duration = 200;
-int red_duration = 300;
-int disabled_duration = 600;
 
 // loop to check for stuff and send stuff
 void loop()
 {
-    // Initialize class_index to an invalid value
-    int class_index = -1;
-
-    // output variable
-    String LEDOutput;
-    String delayDuration;
-
-    // temp variables to compare when lights change
-    int currentGreenState = LEDGreenState;
-    int currentPurpleState = LEDPurpleState;
-    int currentRedState = LEDRedState;
-    int unmappedIndex = -1;
-
     // measure distance
     RangeInCentimeters = ultrasonic.MeasureInCentimeters(); // Measure the distance
 
-    // Receive data from Python
-    if (Serial.available() > 0)
-    {
-        unmappedIndex = Serial.parseInt(); // Read the integer sent from Python
-        class_index = mapLabels(unmappedIndex);
-        int ultrasonic_index = (RangeInCentimeters <= range_threshold) ? 2 : (prev_range_index == 2) ? 1
-                                                                                                     : 0;
+    // Log the range
+    Serial.print(RangeInCentimeters);
+    Serial.println();
 
-        // if prev_class_index light = red, new class_index = green, change class_index to 9 to show amber light first
-        class_index = (prev_class_index == 2 && class_index == 0) ? 9 : class_index;
-
-        // TODO: fix light stuck at amber after pedestrians detected
-        if (ultrasonic_index <= class_index)
-        {
-            switch (class_index)
-            {
-            case 0:
-                LEDRedState = 0;
-                LEDPurpleState = 0;
-                LEDGreenState = 1;
-                LEDOutput = "Green";
-                break;
-            case 1:
-                // maybe change later, not really sure in what case car appears
-                LEDRedState = 1;
-                LEDPurpleState = 0;
-                LEDGreenState = 0;
-                LEDOutput = "Red";
-                delayDuration = "3";
-                break;
-            case 2:
-                LEDRedState = 1;
-                LEDPurpleState = 0;
-                LEDGreenState = 0;
-                LEDOutput = "Red";
-                delayDuration = "2";
-                break;
-            case 3:
-                LEDRedState = 1;
-                LEDPurpleState = 0;
-                LEDGreenState = 0;
-                LEDOutput = "Red";
-                // delay(disabled_duration);
-                delayDuration = "6";
-                break;
-            case 4:
-                LEDRedState = LEDPurpleState = LEDGreenState = 1; // maybe replace by a method that lets LEDs flash
-                LEDOutput = "Crash";
-                break;
-            case 9:
-                LEDRedState = 0;
-                LEDPurpleState = 1;
-                LEDGreenState = 0;
-                LEDOutput = "Orange";
-                break;
-            }
-        }
-        else
-            switch (ultrasonic_index)
-            {
-            case 0:
-                LEDRedState = 0;
-                LEDPurpleState = 0;
-                LEDGreenState = 1;
-                LEDOutput = "Green";
-                break;
-            case 1:
-                LEDRedState = 0;
-                LEDPurpleState = 1;
-                LEDGreenState = 0;
-                LEDOutput = "Orange";
-                break;
-            case 2:
-                LEDRedState = 1;
-                LEDPurpleState = 0;
-                LEDGreenState = 0;
-                LEDOutput = "Red";
-                break;
-            }
-
-        // switching LEDs on/off
-        digitalWrite(LEDGreen, LEDGreenState);
-        digitalWrite(LEDPurple, LEDPurpleState);
-        digitalWrite(LEDRed, LEDRedState);
-
-        prev_class_index = class_index;
-        prev_range_index = ultrasonic_index;
-        // send data to server whenever light changes
-        if (
-            ((currentGreenState != LEDGreenState) || (currentPurpleState != LEDPurpleState) || (currentRedState != LEDRedState)))
-        {
-            if (LEDOutput != "" && class_index > -1 && RangeInCentimeters > 0)
-            {
-                Serial.print(LEDOutput + ",");
-                Serial.print(String(unmappedIndex + 1) + ",");
-                Serial.print(String(RangeInCentimeters) + ",");
-                Serial.print("delayDuration: " + delayDuration + "s");
-                Serial.println();
-            }
-            else
-            {
-
-                Serial.println("No change");
-            }
-        }
-        else
-        {
-            Serial.println("No change");
-        }
-    }
-}
-
-// priority:
-// 0 Not Pedestrian or Vehicles
-// 1 Road Vehicles
-// 2 Pedestrians
-// 3 Disabled Pedestrians
-// 4 Accident/Breakdown
-
-int mapLabels(int i)
-{
-    if (i == 0)
-    {
-        return 4; // Accident/Breakdown
-    }
-    else if (i == 1)
-    {
-        // maybe change later, not really sure in what case car appears
-        return 1; // Road Vehicles
-    }
-    else if (i == 2)
-    {
-        return 2; // Pedestrians
-    }
-    else if (i == 3)
-    {
-        return 0; // Not Pedestrian or Vehicles
-    }
-    else if (i == 4)
-    {
-        return 3; // Disabled Pedestrians
-    }
-    return -1;
+    // Add a delay to avoid overwhelming the serial port
+    delay(500);  // Adjust the delay time as needed
 }
--- server.py
+++ server.py
@@ -1,104 +1,68 @@
-# server.py acts as the central hub that processes the logic of the webcam using Machine Learning
-# import numpy as np
-# teachable machine library
-from teachable_machine import TeachableMachine
-
-# opencv library: computer vision
 import cv2 as cv
-
-# time library: to wait between taking screenshots, can remove whenever
-import time
-
-# serial library: to send data to arduino
+from teachable_machine import TeachableMachine
 import serial
-import json
-import time
-
-# VideoCapture to use webcam camera, 0 is default webcam on laptop, but usb webcam will override
-#   (not sure if its the webcam software or the laptop software that does the override but we take those)
-cap = cv.VideoCapture(0)
 
 # Load the Teachable Machine Keras model
 model = TeachableMachine(
     model_path="./ML_Model/keras_model.h5", labels_file_path="./ML_Model/labels.txt"
 )
 
-# serial port to send data to arduino
-ser = serial.Serial("COM3", 9600)
+# Serial port to send data to Arduino
+arduino_port = "COM8"  # Change this to the correct port
+ser = serial.Serial(arduino_port, 9600)
 
+# VideoCapture to use the webcam camera, 0 is the default webcam on a laptop, but USB webcam will override
+cap = cv.VideoCapture(0)
 
-# declare and store image somewhere
+# Declare and store the image somewhere
 image_path = "./screenshot.jpg"
 
-data_file = open("data.csv", "a")
+# Open a data file to append data
+data_file_path = "data.csv"
+data_file = open(data_file_path, "a")
 cur_data = ""
 
-server_start_time = time.time()
-light_start_time = time.time()
+
+
+
+print('Starting program')
 
 while True:
-    # read the image from the webcam
+    # Read a line from the serial port, as bytes
+    line = ser.readline()
+
+    # Decode bytes to string
+    line = line.decode("utf-8")
+
+    # Remove whitespace before and after the message
+    line = line.strip()
+    print(line + ' cm')
+
+    # Always display the webcam feed
     _, img = cap.read()
-    # save the image to the image_path
-    cv.imwrite(image_path, img)
-
-    # make predictions using the model
-    result = model.classify_image(image_path)
-
-    print("class_name:", result["class_name"])
-    print("class_confidence: {:.2f}".format(round(result["class_confidence"], 2)))
-
-    ser.write(str(result["class_index"]).encode())
-
-    # display the image
     cv.imshow("Webcam", img)
+    
+    # If the read was successful and the range is less than 150, scan the image
+    if int(line) <= 150:
+        # Save the image to the image_path
+        cv.imwrite(image_path, img)
 
-    # if q is pressed, break out of the loop
+        # Make predictions using the model
+        result = model.classify_image(image_path)
+
+        print("Class name:", result["class_name"])
+        print("Class confidence: {:.2f}".format(round(result["class_confidence"], 2)))
+
+        # write the data to the file
+        cur_data = line + "," + result["class_name"] + "," + str(result["class_confidence"]) + "\n"
+        data_file.write(cur_data)
+
+    # If 'q' is pressed, break out of the loop
     if cv.waitKey(1) & 0xFF == ord("q"):
         break
 
-    # read a line from the serial port, as bytes
-    line = ser.readline()
-
-    # # read a line from the serial port, decode bytes to string
-    line = line.decode("utf-8")
-
-    # message should end with \n, this removes the \n and all other whitespace before and after the message
-    line = line.strip()
-
-    # if the read was successful, do something with it
-    if line == "No change":
-        print(line)
-
-    elif line and line != "":
-        print(f"=== ARDUINO DATA START ===")
-        print(f"{line}")
-        # print("Time:", time.time())
-
-        # light has been on for X seconds since server started and current time
-        light_time = time.time() - light_start_time
-        # set current time as new light start time
-        light_start_time = time.time()
-
-        # recording of data
-        print(line.split(","))
-        light_color, class_index, range_in_centimeters, delay = line.split(",")
-        data = {
-            "timestamp": round(time.time()),
-            "class_index": class_index,
-            "range_in_centimeters": range_in_centimeters,
-            "light_color": light_color,
-            "time_light_has_been_on": light_time,
-        }
-
-        filename = "./logs/data.json"
-
-        with open(filename) as file:
-            listObj = json.load(file)
-
-        listObj.append(data)
-
-        with open(filename, "w") as json_file:
-            json.dump(listObj, json_file, separators=(",", ": "))
-
-        print(f"=== ARDUINO DATA END ===\n")
+# Release resources
+cap.release()
+cv.destroyAllWindows()
+ser.close()
+data_file.close()
